58a442b6619c5d3cd40a493c9813630727904df4
Sneekyboots <sneekyboots122@gmail.com>
2024-05-15 23:59:04 +0530

    done

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+ let x: i32=16;
+ println!("{}",x);
+    let z:String =String::from("Hello soroban");
+    let y: &str = "Hello World";
+    println!("{y}");
+
+    println!("{z}");
+
+}
